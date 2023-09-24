fc63115e04e26fb0185f716c60a1cf9f5721f996
HeshamMoawad <heshammoawad120120@yahoo.com>
2023-09-24 10:46:25 +0300

    fix ExcelReader and add some methods

--- readers.py
+++ readers.py
@@ -1,31 +1,42 @@
+import typing , os
 import pandas as pd 
-# from tasks import QThread   
-class Excel ():
-    
-    def Load_Exel(self):
-        data = pd.read_excel("Book1.xlsx")
-        data=data["Faill Num"]
-        data=(list(data))
-        print(data)
-        return data
-              
-    def ExcelRead(self,code, Num):
-        Data = Excel.Load_Exel()
-      
-        resultdata = list(Data)
-        if len(str(code)) == 1:
-                code = "0" + str(code)
-                resultdata.append(str(code) + str(Num))
-        elif len(str(code)) == 2:
-                resultdata.append(str(code) + str(Num))
-        else :
-                resultdata    
+from tasks import  QThread , pyqtSignal , typing
 
-        Data = pd.DataFrame({"code": code ,"Num":Num ,"Faill num": resultdata})
+
+# Constants
+COLUMNS = ['AreaCode','PhoneNumber']
+TABEL_MODEL_COLUMNS = COLUMNS + ["Invoice Price","Server Message"]
+
+
+
+
+class ExcelReader(QThread):
+    # Define Signals
+    onReadExcel = pyqtSignal(pd.DataFrame)
+    onFaildRead = pyqtSignal(str)
+
+    def __init__(self, excelPath:str = '') -> None:
+        super().__init__()
+        self.excelPath = excelPath
+
+    def setExcelPath(self , excelPath:str):
+        self.excelPath = excelPath
+
+    # Read Excel Method
+    def loadExel(self)-> pd.DataFrame :
+        if os.path.isfile(self.excelPath):
+            try :
+                data = pd.read_excel(self.excelPath)
+                self.onReadExcel.emit(data)
+            except Exception as e :
+                self.onFaildRead.emit(e)
+        else :
+            self.onFaildRead.emit("File Dos Not Exist or Path Not Found !!")
+
+    # Method that Run When QThread Started
+    def run(self) -> None:
+        self.loadExel()
         
-        Data.to_excel("data.xlsx", index=False)
-        
- 
 
  
  
